1e7b4a4dbdf8613d8f29c29b1dd48dd8462395a8
davidramiro <david@kore.cc>
2024-03-25 12:56:09 +0100

    fix: use 5:6:5 rgb values

--- src/Display.h
+++ src/Display.h
@@ -6,13 +6,13 @@
 #include <Adafruit_NeoPixel.h>
 #include <ArduinoLog.h>
 
-const uint16_t BLACK = uint16_t(0x0);
-const uint16_t GREEN = uint16_t(0x00FF00);
-const uint16_t BLUE = uint16_t(0x0000FF);
-const uint16_t RED = uint16_t(0xFF0000);
-const uint16_t ORANGE = uint16_t(0xFF8800);
-const uint16_t WHITE = uint16_t(0xFFFFFF);
-const uint16_t PURPLE = uint16_t(0xAF00AF);
+const uint16_t BLACK = 0x0;
+const uint16_t GREEN = 0x07E0;
+const uint16_t BLUE = 0x001F;
+const uint16_t RED = 0xF800;
+const uint16_t ORANGE = 0xFCA0;
+const uint16_t WHITE = 0xFFFF;
+const uint16_t PURPLE = 0xF81F;
 
 class Display {
 public:
